9ebebb4fcd7600ab81c19f7833d5a1cf59e9cdee
NataliyaSkorska <60364946+NataliyaSkorska@users.noreply.github.com>
2020-01-28 01:45:16 +0200

    Add files via upload

---
+++ HW2 Part2.py
@@ -0,0 +1,14 @@
+a = "It's not the cough that will carry you off!"
+
+print (a)
+
+# Concatenation
+
+a = "What"
+b = "was"
+c = "the"
+d = "story"
+x = "?"
+y = a + " " + b + " " + c + " " + d + "?"
+
+print (y)
